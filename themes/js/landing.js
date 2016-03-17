7e68c1d2412524df669ff710d67c3bcca684f534
DanMburu <danstjoy@gmail.com>
2016-03-17 14:21:53 +0300

    asas

--- themes/js/landing.js
+++ themes/js/landing.js
@@ -8,8 +8,8 @@ function onDeviceReady() {
     db = window.openDatabase("Radioson", "1.0", "PhoneGap Demo", 200000);
     var firstrun = window.localStorage.getItem("runned");
     //  firstrun = null;
-   // alert(firstrun);
-    if ( firstrun == null ) {
+    alert(firstrun);
+    if (firstrun == null ) {
         setTimeout(function(){hideLoader(); }, 2000);
         $('#landing').fadeIn();
         populateDB();
